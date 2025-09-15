136e44db861167d66f64871d66e8aade3ad85294
gregorsajovic <gregor.sajovic@gmail.com>
2025-09-15 17:52:27 +0200

    csv files

--- src/learnpy3/17_csv_files.py
+++ src/learnpy3/17_csv_files.py
@@ -44,3 +44,27 @@ for row in reader:
 print(header)
 print(data[0])
 
+# In finance -> Stock Return = % change in price from one day to the next
+# Common time frames:
+# >> Daily
+# >> Weekly
+# >> Monthly
+# >> Annual
+
+# Compute and store dily stock returns
+returns_path = "./data/google_stock_data_with_returns.csv"
+file = open(returns_path, 'w')
+writer = csv.writer(file)
+writer.writerow(["Date", "Return"])
+
+for i in range(len(data)-1):
+ todays_row = data[i]
+ todays_date = todays_row[0]
+ todays_price = todays_row[-1]
+ yesterdays_row = data[i+1]
+ yesterdays_price = yesterdays_row[-1]
+
+ daily_return = (todays_price - yesterdays_price) / yesterdays_price
+ formated_date = todays_date.strftime('%m/%d/%Y')
+ writer.writerow([formated_date, daily_return])
+
